0fd074059b19993e3d0d9f8529fce55a90d66998
Austin Vern Songer <austin.v.songer@gmail.com>
2015-05-08 14:11:10 -0500

    Spelling

--- HW1.java
+++ HW1.java
@@ -17,7 +17,7 @@ public class AgeIn5
 		name = keyboard.next();
 		System.out.print( "Hi, " + name + "! How old are you? " );
 		age = keyboard.nextInt();
-		System.out.println( "\nDid you know that in five years you will be " + (age + 5) + " years old?" );
+		System.out.println( "Did you know that in five years you will be " + (age + 5) + " years old?" );
 		System.out.println( "And five years ago you were " + (age - 5) + "! Imagine that!" );
 	}
 }
